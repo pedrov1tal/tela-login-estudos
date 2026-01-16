e26e39f65ec0995ef31c16ca196e869b2c60e0e4
Pedro Vital Machado <pedro.machadoo@hotmail.com>
2026-01-16 05:36:33 -0300

    Fim do projeto de estudos

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -16,11 +16,11 @@ const Login = () => {
    <div className="container">
     <form onSubmit={handleSubmit}>
         <h1>Acesse o sistema</h1>
-        <div>
+        <div className="input-field">
         <input type="email" placeholder='email' onChange={(e) => setUsername(e.target.value)}/>  
         <FaUser className="icon"></FaUser>
         </div>
-        <div>
+        <div className="input-field">
         <input type="password" placeholder='Senha' onChange={(e) => setPassword(e.target.value)} />
         <FaLock className="icon"></FaLock>
         </div>
