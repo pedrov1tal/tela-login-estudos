3de034f9faee0880af3529db25d981c9e061a2fc
Pedro Vital Machado <pedro.machadoo@hotmail.com>
2026-01-16 04:55:48 -0300

    Inicio do projeto de estudos

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { LogIn } from 'lucide-react'
+import './App.css'
+import Login from './components/login/Login'
+
+function App() {
+ 
+
+  return (
+    <>
+     <div className="App">
+      <Login></Login>
+     </div>
+    </>
+  )
+}
+
+export default App
